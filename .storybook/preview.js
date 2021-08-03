ed1ac1fb94aa7765f81f2aed9cf6f91fa630ed3a
Daniel Silveira <danielzinhors@gmail.com>
2021-08-02 22:41:02 -0300

    Ajuste do storybook e inicio da logo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,19 +3,26 @@ import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
-    }
-  },
-  decorators: [
-    (Story) => (
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
-    )
-  ]
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   moduleNameMapper: {
     styled_components:
       '<rootDir>/node_module/styled-components/dist/styled-components.browser.cjs.js'
