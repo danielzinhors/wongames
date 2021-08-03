577dd3ef86c16c918857985677c543ee7b0b34ba
Daniel Silveira <danielzinhors@gmail.com>
2021-08-03 20:37:53 -0300

    logo tdd 2 parte

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,8 +1,22 @@
 import styled, { css } from 'styled-components'
 import { LogoProps } from '.'
 
+const wapperModifiers = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.3rem;
+  `,
+
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color = 'white' }) => css`
-    color: ${theme.colors[color]};
+  ${({ theme, color, size }) => css`
+    color: ${theme.colors[color!]};
+
+    ${!!size && wapperModifiers[size]};
   `}
 `
