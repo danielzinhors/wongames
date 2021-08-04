1a7dd57a7e311552c03dd018db1d98dac9d508fe
Daniel Silveira <danielzinhors@gmail.com>
2021-08-03 21:09:00 -0300

    logo final

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,7 +1,8 @@
 import styled, { css } from 'styled-components'
 import { LogoProps } from '.'
+import media from 'styled-media-query'
 
-const wapperModifiers = {
+const wrapperModifiers = {
   normal: () => css`
     width: 11rem;
     height: 3.3rem;
@@ -10,13 +11,31 @@ const wapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
-    ${!!size && wapperModifiers[size]};
+    ${!!size && wrapperModifiers[size]};
+
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile};
   `}
 `
