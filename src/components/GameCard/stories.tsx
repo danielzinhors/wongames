db1eb2c79265188b059536a6369437e857cdc860
Daniel Silveira <danielzinhors@gmail.com>
2021-08-25 21:41:28 -0300

    create comp gamecard

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,40 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/whillianjusten/300x140',
+    price: 'RS 236,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  argTypes: {
+    onFav: {
+      action: 'clicked'
+    },
+    ribbon: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+export const WhithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WhithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
