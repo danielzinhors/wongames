257c6697eeff97fed90a695c77528b8bfcedb1d9
Daniel Silveira <danielzinhors@gmail.com>
2021-09-27 21:53:03 -0300

    Create comp GameItem

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameItem, { GameItemProps } from '.'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Read Dead Redenption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const Default: Story<GameItemProps> = (args) => <GameItem {...args} />
+
+export const WithPayment: Story<GameItemProps> = (args) => (
+  <GameItem {...args} />
+)
+
+WithPayment.args = {
+  downloadLink: 'https://wongames.com/downloads/reddead2',
+  paymentInfo: {
+    number: '**** **** **** 4326',
+    flag: 'mastercard',
+    img: '/img/cards/mastercard.png',
+    puschaseDate: 'Puschase made on 07/20/2020 at 20:32'
+  }
+}
---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameItem from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Read Dead Redenption 2',
+  price: 'R$ 215,00'
+}
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+    expect(
+      screen.getByRole('heading', { name: /Read Dead Redenption 2/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/R\$ 215,00/i)).toBeInTheDocument()
+  })
+
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
+
+  it('should render the payment info', () => {
+    const paymentInfo = {
+      number: '**** **** **** 4326',
+      flag: 'mastercard',
+      img: '/img/cards/mastercard.png',
+      puschaseDate: 'Puschase made on 07/20/2020 at 20:32'
+    }
+    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
+
+    expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
+      'src',
+      paymentInfo.img
+    )
+
+    expect(screen.getByText(paymentInfo.number)).toBeInTheDocument()
+    expect(screen.getByText(paymentInfo.puschaseDate)).toBeInTheDocument()
+  })
+})
