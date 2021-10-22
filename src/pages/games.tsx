1dfe998f68ea467a5dc71e4346c9eb6df95372e1
Daniel Silveira <danielzinhors@gmail.com>
2021-10-22 19:43:17 -0300

    add apollo nas dev dep

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,34 @@
 import GamesTemplate, { GameTemplateProps } from 'templates/Games'
-import mockFilter from 'components/ExploreSidebar/mock'
-import gemesMock from 'components/GameCardSlider/mock'
+import filterItemsMock from 'components/ExploreSidebar/mock'
+//import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 export default function GamesPage(props: GameTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
+
   return {
     props: {
-      games: gemesMock,
-      filterItems: mockFilter
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })), //gamesMock
+      filterItems: filterItemsMock
     }
   }
 }
