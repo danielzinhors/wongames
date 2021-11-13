d87a9a25d3a53b517d26aaee40e84b47a6118886
Daniel Silveira <danielzinhors@gmail.com>
2021-11-13 16:47:41 -0300

    Pegando os banners da api para a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import bannerSliderMock from 'components/BannerSlider/mock'
 import gamesCardMock from 'components/GameCardSlider/mock'
 import highligthMock from 'components/Highlight/mock'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home '
 
 import Home, { HomeTemplateProps } from 'templates/Home'
+import { initializeApollo } from 'utils/apollo'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -14,10 +16,25 @@ export default function Index(props: HomeTemplateProps) {
 // getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
 // getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
   return {
     props: {
-      banners: bannerSliderMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text, // utilza || null, ou com destructor
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gamesCardMock,
       mostPopularHighlight: highligthMock,
       mostPopularGames: gamesCardMock,
