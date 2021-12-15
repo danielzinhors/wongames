a872b1490288f0562b01d1fc995171eee3385ad5
Daniel Silveira <danielzinhors@gmail.com>
2021-12-15 17:11:38 -0300

    Ajustando Calback da rota

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -20,7 +20,8 @@ const FormSignIn = () => {
   const [formError, setFormError] = useState('')
   const [values, setValues] = useState({ email: '', password: '' })
   const [loading, setLoading] = useState(false)
-  const { push } = useRouter()
+  const routes = useRouter()
+  const { push, query } = routes
   const [fieldError, setFieldError] = useState<FieldErrors>({})
 
   const handleSubmit = async (event: React.FormEvent) => {
@@ -39,7 +40,7 @@ const FormSignIn = () => {
     const result = await signIn('credentials', {
       ...values,
       redirect: false,
-      callbackUrl: '/'
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
     })
 
     if (result?.url) {
