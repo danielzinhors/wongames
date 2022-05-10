a310fb7a5b3033f4610ee36c7ca0d00c4b11d3b2
danielzinhors <danielzinhors@gmail.com>
2022-05-09 21:30:59 -0300

    Testes dos order by  pag explore com Cypress

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -7,8 +7,11 @@ import {
 } from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
-  it('should render filter columns', () => {
+  before(() => {
     cy.visit('/games')
+  })
+
+  it('should render filter columns', () => {
 
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
@@ -23,5 +26,28 @@ describe('Explore Page', () => {
 
     cy.getFields(sortFields)
 
+  })
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
+  })
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('FREE').should('exist')
+    })
+    cy.findByText(/highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText(/^\$\d+(\.\d{1,2})?/)
+        .invoke('text') //transforma em texto
+        .then( $el => $el.replace('$', ''))
+        .then(parseFloat)
+        .should('be.gt', 0)
+    })
   })
 })
