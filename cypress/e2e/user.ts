b96243c9b515751655932d2e3c3254195bdd2e7a
Daniel Silveira <danielzinhors@gmail.com>
2023-04-16 13:02:36 -0300

    create teste de redirect profileme sem login para o signin

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,7 +3,7 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  it.skip('should sign up', () => {
+  it('should sign up', () => {
 
     const user = createUser()
     cy.visit('/sign-up')
@@ -23,6 +23,16 @@ describe('User', () => {
 
     cy.findByRole('link', { name: /sign in/i }).should('exist')
     cy.findByText(/cypress/i).should('not.exist')
+  })
+
+  it.only('should sign the user and redirect to the page that it was defined previously', () => {
+    cy.visit('/profile/me')
+    cy.url().should('contain', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`)
+
+    cy.signIn()
+    cy.url().should('contain', `${Cypress.config().baseUrl}/profile/me`)
+    cy.findByLabelText(/username/i).should('have.value', 'cypress')
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com')
   })
 
 });
